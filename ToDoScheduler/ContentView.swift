2a2f4006b921f65e5e09abbcf452c0d3c429d171
camlostshi <camlost4036@gmail.com>
2023-02-05 01:46:45 +0800

    Initial Commit

---
+++ ToDoScheduler/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ToDoScheduler
+//
+//  Created by Camlost 施 on 2023/2/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ToDoScheduler/ToDoSchedulerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoSchedulerApp.swift
+//  ToDoScheduler
+//
+//  Created by Camlost 施 on 2023/2/5.
+//
+
+import SwiftUI
+
+@main
+struct ToDoSchedulerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
