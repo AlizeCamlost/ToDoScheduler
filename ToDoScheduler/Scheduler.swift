f711a350439f49ea63768cbb09b137790d190cc8
camlostshi <camlost4036@gmail.com>
2023-02-05 01:59:05 +0800

    MVVM structure

---
+++ ToDoScheduler/Scheduler.swift
@@ -0,0 +1,9 @@
+//
+//  Scheduler.swift
+//  ToDoScheduler
+//
+//  Created by Camlost 施 on 2023/2/5.
+//
+//  This is the ViewModel of MVVM
+
+import SwiftUI
---
+++ ToDoScheduler/SchedulerStruct.swift
@@ -0,0 +1,9 @@
+//
+//  SchedulerStruct.swift
+//  ToDoScheduler
+//
+//  Created by Camlost 施 on 2023/2/5.
+//
+//  This is the Model of MVVM
+
+import Foundation
