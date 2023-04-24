a32c188b2668e8c7807a74a89e8eb802a4ff658a
CamlostShi <camlost4036@gmail.com>
2023-04-25 03:21:06 +0800

    combine storyboard(trying)

---
+++ ToDoScheduler/fromStoryboard/SetWeekCell.swift
@@ -0,0 +1,19 @@
+//
+//  SetWeekCell.swift
+//  EasySchedule
+//
+//  Created by 包娟 on 2023/4/9.
+//  Copyright © 2023 包娟. All rights reserved.
+//
+
+import UIKit
+
+class SetWeekCell: UICollectionViewCell {
+    var choosen = false
+    
+    @IBOutlet weak var topBorder: UIView!
+    @IBOutlet weak var weekNumLabel: UILabel!
+    
+    @IBOutlet weak var leftBorder: UIView!
+
+}
